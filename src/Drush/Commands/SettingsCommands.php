8d0651e47e1bae8ef7ae34586f0be3e2805f7616
Mike Decker <pookmish@stanford.edu>
2025-03-24 09:21:50 -0700

    Settings command

--- src/Drush/Commands/SettingsCommands.php
+++ src/Drush/Commands/SettingsCommands.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Drupal\SwsDrush\Drush\Commands;
 
 use Drush\Utils\StringUtils;
-use Robo\Contract\VerbosityThresholdInterface;
 use Drush\Attributes as CLI;
 use Drush\Commands\DrushCommands;
 
@@ -14,46 +13,61 @@ use Drush\Commands\DrushCommands;
  */
 final class SettingsCommands extends DrushCommands {
 
+  use SwsCommandsTrait;
+
   /**
    * Generates default settings files for Drupal and drush.
    */
   #[CLI\Command(name: 'source:build:settings', aliases: ['settings'])]
-  public function buildSettings() {
+  #[CLI\Option(name: 'db-port', description: 'Database Port')]
+  #[CLI\Option(name: 'db-host', description: 'Database Host')]
+  #[CLI\Option(name: 'db-user', description: 'Database User')]
+  #[CLI\Option(name: 'db-pass', description: 'Database Password')]
+  #[CLI\Option(name: 'db-name', description: 'Database Name')]
+  public function buildSettings(array $options = [
+    'db-port' => 3306,
+    'db-host' => 'localhost',
+    'db-user' => 'user',
+    'db-pass' => 'password',
+    'db-name' => 'drupal',
+    'multisites' => ['default'],
+  ]
+  ) {
+    $sites_dir = $this->getDir() . '/docroot/sites';
 
     // Generate hash file in salt.txt.
     $this->hashSalt();
 
-    $default_multisite_dir = $this->getConfigValue('docroot') . "/sites/default";
-    $default_project_default_settings_file = "$default_multisite_dir/default.settings.php";
+    $default_project_default_settings_file = "$sites_dir/default/default.settings.php";
+    $blt_local_settings_file = __DIR__ . '/../../../settings/default.local.settings.php';
+    $blt_includes_settings_file = __DIR__ . '/../../../settings/default.includes.settings.php';
+    $blt_glob_settings_file = __DIR__ . '/../../../settings/default.global.settings.php';
+    $blt_local_drush_file = __DIR__ . '/../../../settings/default.local.drush.yml';
 
-    $multisites = $this->getConfigValue('multisites');
-    $initial_site = $this->getConfigValue('site');
+    $xpand_config = [
+      '${db.port}' => $options['db-port'],
+      '${db.host}' => $options['db-port'],
+      '${db.user}' => $options['db-port'],
+      '${db.pass}' => $options['db-port'],
+    ];
 
-    $this->logger->debug("Multisites found: " . implode(',', $multisites));
-    $this->logger->debug("Initial site: $initial_site");
-
-    foreach ($multisites as $multisite) {
+    foreach ($options['multisites'] as $multisite) {
       // Generate settings.php.
-      $multisite_dir = $this->getConfigValue('docroot') . "/sites/$multisite";
+      $multisite_dir = "/$sites_dir/$multisite";
       $project_default_settings_file = "$multisite_dir/default.settings.php";
       $project_settings_file = "$multisite_dir/settings.php";
 
       // Generate local.settings.php.
-      $blt_local_settings_file = $this->getConfigValue('blt.root') . '/settings/default.local.settings.php';
       $default_local_settings_file = "$multisite_dir/settings/default.local.settings.php";
       $project_local_settings_file = "$multisite_dir/settings/local.settings.php";
 
       // Generate default.includes.settings.php.
-      $blt_includes_settings_file = $this->getConfigValue('blt.root') . '/settings/default.includes.settings.php';
       $default_includes_settings_file = "$multisite_dir/settings/default.includes.settings.php";
 
       // Generate sites/settings/default.global.settings.php.
-      $blt_glob_settings_file = $this->getConfigValue('blt.root') . '/settings/default.global.settings.php';
-      $default_glob_settings_file = $this->getConfigValue('docroot') . "/sites/settings/default.global.settings.php";
-      $global_settings_file = $this->getConfigValue('docroot') . "/sites/settings/global.settings.php";
+      $default_glob_settings_file = $this->getDir() . "docroot/sites/settings/default.global.settings.php";
 
       // Generate local.drush.yml.
-      $blt_local_drush_file = $this->getConfigValue('blt.root') . '/settings/default.local.drush.yml';
       $default_local_drush_file = "$multisite_dir/default.local.drush.yml";
       $project_local_drush_file = "$multisite_dir/local.drush.yml";
 
@@ -63,6 +77,9 @@ final class SettingsCommands extends DrushCommands {
         $blt_includes_settings_file => $default_includes_settings_file,
         $blt_local_drush_file => $default_local_drush_file,
         $default_local_drush_file => $project_local_drush_file,
+        $blt_glob_settings_file => $default_glob_settings_file,
+        $default_project_default_settings_file => $project_default_settings_file,
+        $project_default_settings_file => $project_settings_file,
       ];
       // Define an array of files that require property expansion.
       $expand_map = [
@@ -70,85 +87,36 @@ final class SettingsCommands extends DrushCommands {
         $default_local_drush_file => $project_local_drush_file,
       ];
 
-      // Add default.global.settings.php if global.settings.php does not exist.
-      if (!file_exists($global_settings_file)) {
-        $copy_map[$blt_glob_settings_file] = $default_glob_settings_file;
-      }
-
-      // Only add the settings file if the default exists.
-      if (file_exists($default_project_default_settings_file)) {
-        $copy_map[$default_project_default_settings_file] = $project_default_settings_file;
-        $copy_map[$project_default_settings_file] = $project_settings_file;
-      }
-      elseif (!file_exists($project_settings_file)) {
-        $this->logger->warning("No $default_project_default_settings_file file found.");
-      }
-
-      $task = $this->taskFilesystemStack()
-        ->stopOnFail()
-        ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_VERBOSE)
-        ->chmod($multisite_dir, 0777);
-
-      if (file_exists($project_settings_file)) {
-        $task->chmod($project_settings_file, 0777);
-      }
-
       // Copy files without overwriting.
       foreach ($copy_map as $from => $to) {
         if (!file_exists($to)) {
-          $task->copy($from, $to);
+          copy($from, $to);
         }
       }
 
-      $result = $task->run();
-
-      foreach ($expand_map as $from => $to) {
-        $this->getConfig()->expandFileProperties($to);
+      foreach ($expand_map as $to) {
+        $file_contents = file_get_contents($to);
+        $file_contents = str_replace(array_keys($xpand_config), array_values($xpand_config), $file_contents);
+        file_put_contents($to, $file_contents);
       }
 
-      if (!$result->wasSuccessful()) {
-        throw new \Exception("Unable to copy files settings files from BLT into your repository.");
-      }
-
-      $result = $this->taskWriteToFile($project_settings_file)
-        ->appendUnlessMatches('#vendor/acquia/blt/settings/blt.settings.php#', 'require DRUPAL_ROOT . "/../vendor/acquia/blt/settings/blt.settings.php";' . "\n")
-        ->appendUnlessMatches('#Do not include additional settings here#', $this->settingsWarning . "\n")
-        ->append(TRUE)
-        ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_VERBOSE)
-        ->run();
-
-      if (!$result->wasSuccessful()) {
-        throw new \Exception("Unable to modify $project_settings_file.");
-      }
-
-      $result = $this->taskFilesystemStack()
-        ->chmod($project_settings_file, 0644)
-        ->stopOnFail()
-        ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_VERBOSE)
-        ->run();
-
-      if (!$result->wasSuccessful()) {
-        $this->getInspector()
-          ->getFs()
-          ->makePathRelative($project_settings_file, $this->getConfigValue('repo.root'));
-        throw new \Exception("Unable to set permissions on $project_settings_file.");
+      $project_settings_file_contents = file_get_contents($project_settings_file);
+      if (!preg_match('/^.*?sws\.settings\.php/', $project_settings_file_contents)) {
+        $project_settings_file_contents .= PHP_EOL . "require DRUPAL_ROOT . '/../drush/Commands/contrib/sws-drush-commands/settings/sws.settings.php';" . PHP_EOL;
+        file_put_contents($project_settings_file, $project_settings_file_contents);
       }
     }
   }
 
   /**
    * Writes a hash salt to ${repo.root}/salt.txt if one does not exist.
-   *
-   * @command drupal:hash-salt:init
-   * @aliases dhsi setup:hash-salt
-   *
-   * @return int
-   *   A CLI exit code.
-   *
-   * @throws \Acquia\Blt\Robo\Exceptions\BltException
    */
+  #[CLI\Command(name: 'drupal:hash-salt:init', aliases: [
+    'dhsi',
+    'setup:hash-salt',
+  ])]
   public function hashSalt() {
-    $hash_salt_file = $this->getConfigValue('repo.root') . '/salt.txt';
+    $hash_salt_file = $this->getDir() . '/salt.txt';
     if (!file_exists($hash_salt_file)) {
       $this->say("Generating hash salt...");
       file_put_contents($hash_salt_file, StringUtils::generatePassword(55));
