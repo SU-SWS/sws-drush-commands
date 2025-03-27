18ce876c633e5b38e7b24af701d8e71bbbc4d186
Mike Decker <pookmish@stanford.edu>
2025-03-27 13:33:39 -0700

    Adjust sync command

--- src/Drush/Commands/SyncDrushCommands.php
+++ src/Drush/Commands/SyncDrushCommands.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace Drupal\SwsDrush\Drush\Commands;
 
+use Consolidation\Config\Config;
+use Consolidation\Config\Loader\ConfigProcessor;
 use Drupal\SwsDrush\Output\Checklist;
 use Drush\Boot\DrupalBootLevels;
 use Drush\Commands\DrushCommands;
 use Drush\Attributes as CLI;
+use Drush\Config\Loader\YamlConfigLoader;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Filesystem\Path;
 
 /**
  * A Drush command file.
@@ -24,10 +28,11 @@ final class SyncDrushCommands extends DrushCommands {
    * Sync a site from production to local and perform database updates.
    */
   #[CLI\Command(name: 'sws:site:sync', aliases: ['sync'])]
-  #[CLI\Argument(name: 'site_name', description: 'Site name to sync.')]
+  #[CLI\Option(name: 'site_name', description: 'Site name to sync.')]
   #[CLI\Option(name: 'with-files', description: 'Sync files after the database.')]
   #[CLI\Option(name: 'partial', description: 'Run config imports with --partial flag.')]
-  public function syncSite(string $site_name, array $options = [
+  public function syncSite(array $options = [
+    'site' => 'default',
     'with-files' => FALSE,
     'partial' => FALSE,
   ]
@@ -36,20 +41,20 @@ final class SyncDrushCommands extends DrushCommands {
     $outputCallback = $this->getOutputCallback($this->output(), $this->checklist);
 
     $this->checklist->addItem('Syncing database');
-    $this->syncDatabase($outputCallback, $site_name);
+    $this->syncDatabase($outputCallback, $options['site']);
     $this->checklist->completePreviousItem();
 
     $this->checklist->addItem('Sanitize database');
-    $this->sanitizeDatabase($outputCallback, $site_name);
+    $this->sanitizeDatabase($outputCallback, $options['site']);
     $this->checklist->completePreviousItem();
 
     $this->checklist->addItem('Update database');
-    $this->updateDatabase($outputCallback, $site_name, $options['partial']);
+    $this->updateDatabase($outputCallback, $options['site'], $options['partial']);
     $this->checklist->completePreviousItem();
 
     if ($options['with-files']) {
       $this->checklist->addItem('Syncing Files');
-      $this->syncFiles($outputCallback, $site_name);
+      $this->syncFiles($outputCallback, $options['site']);
       $this->checklist->completePreviousItem();
     }
   }
@@ -61,6 +66,8 @@ final class SyncDrushCommands extends DrushCommands {
    *   Multisite name.
    */
   protected function syncDatabase(\Closure $outputCallback, string $site_name) {
+    $remoteAlias = $this->getSiteRemoteAlias($site_name);
+
     $outputCallback('out', "Clearing local database");
     $this->localMachineHelper()->execute([
       'drush',
@@ -71,7 +78,7 @@ final class SyncDrushCommands extends DrushCommands {
     $result = $this->localMachineHelper()->execute([
       'drush',
       'sql-sync',
-      "@$site_name.prod",
+      $remoteAlias,
       "@$site_name.local",
       '-y',
     ], $outputCallback, $this->getDir());
@@ -111,6 +118,13 @@ final class SyncDrushCommands extends DrushCommands {
   protected function updateDatabase(\Closure $outputCallback, string $site_name, bool $partial = FALSE) {
     $outputCallback('out', "Database updates");
     if (!$partial) {
+      $this->localMachineHelper()->execute([
+        'drush',
+        "@$site_name.local",
+        'en',
+        'field_validation_legacy',
+      ], $outputCallback, $this->getDir(), FALSE);
+
       $result = $this->localMachineHelper()->execute([
         'drush',
         "@$site_name.local",
@@ -150,10 +164,11 @@ final class SyncDrushCommands extends DrushCommands {
    *   Multisite name.
    */
   protected function syncFiles(\Closure $outputCallback, string $site_name) {
+    $remoteAlias = $this->getSiteRemoteAlias($site_name);
     $result = $this->localMachineHelper()->execute([
       'drush',
       'rsync',
-      "@$site_name.prod:%files/",
+      "$remoteAlias:%files/",
       "@$site_name.local:%files",
       "--exclude-paths='styles:css:js'",
       '-v',
@@ -166,7 +181,7 @@ final class SyncDrushCommands extends DrushCommands {
     $result = $this->localMachineHelper()->execute([
       'drush',
       'rsync',
-      "@$site_name.prod:%files-private/",
+      "$remoteAlias:%files-private/",
       "@$site_name.local:%files-private",
       "--exclude-paths='styles:css:js'",
       '-v',
@@ -188,8 +203,10 @@ final class SyncDrushCommands extends DrushCommands {
     'sync-files' => [InputOption::VALUE_IS_ARRAY, InputOption::VALUE_REQUIRED],
   ]
   ) {
-    $this->ensureOption('sync-ssh', fn() => $this->io()->ask('SSH string'), true);
-    $this->ensureOption('sync-files', fn() => $this->io()->ask('File to sync'), true);
+    $this->ensureOption('sync-ssh', fn() => $this->io()
+      ->ask('SSH string'), TRUE);
+    $this->ensureOption('sync-files', fn() => $this->io()
+      ->ask('File to sync'), TRUE);
 
     $this->localMachineHelper()->checkRequiredBinariesExist(['rsync']);
 
@@ -214,9 +231,37 @@ final class SyncDrushCommands extends DrushCommands {
    */
   #[CLI\Command(name: 'sws:site:sync-files')]
   #[CLI\Argument(name: 'site_name', description: 'Site name to sync.')]
-  public function syncSiteFiles($site_name) {
+  public function syncSiteFiles(string $site_name) {
     $outputCallback = $this->getOutputCallback($this->output(), $this->checklist);
     $this->syncFiles($outputCallback, $site_name);
+  }
+
+  /**
+   * Get the remote alias of a site using the sws.yml file in the site dir.
+   *
+   * @param string $siteName
+   *   Site machine name.
+   *
+   * @return string
+   *   Drush remote alias.
+   */
+  protected function getSiteRemoteAlias(string $siteName): string {
+    $fileSystem = $this->localMachineHelper()->getFilesystem();
+    $configFile = Path::join($this->getDir(), 'docroot', 'sites', $siteName, 'sws.yml');
+    $remoteAlias = "@$siteName.prod";
+
+    if ($fileSystem->exists($configFile)) {
+      $config = new Config();
+      $loader = new YamlConfigLoader();
+      $processor = new ConfigProcessor();
+      $processor->extend($loader->load($configFile));
+      $config->replace($processor->export());
+      $ra = $config->get('site.remote-alias');
+      if ($ra) {
+        $remoteAlias = $ra;
+      }
+    }
+    return str_starts_with($remoteAlias, '@') ? $remoteAlias : "@$remoteAlias";
   }
 
 }
